a580565d008309b5e35a63c46df0b2c1b9e96950
joxiplayer <joxiplayer.id@gmail.com>
2022-01-06 13:15:13 +0700

    first commit

--- Download/Design-Daily-Task/script.js
+++ Download/Design-Daily-Task/script.js
@@ -26,41 +26,6 @@ downloadButton.addEventListener("click", () => {
         }
     }, 1000);
 });
-  $(document).ready(function () {
-      $("button").click(function (event) {
-          event.preventDefault();
-          CopyToClipboard("Download Design Daily Task | Stack Image Link : https://stackimage.github.io/Download/Design-Daily-Task/", true, "Value copied");
-      });
-  });
-
-  function CopyToClipboard(value, showNotification, notificationText) {
-      var $temp = $("<input>");
-      $("body").append($temp);
-      $temp.val(value).select();
-      document.execCommand("copy");
-      $temp.remove();
-
-      if (typeof showNotification === 'undefined') {
-          showNotification = true;
-      }
-      if (typeof notificationText === 'undefined') {
-          notificationText = "Copied to clipboard";
-      }
-
-      var notificationTag = $("div.copy-notification");
-      if (showNotification && notificationTag.length == 0) {
-          notificationTag = $("<div/>", { "class": "copy-notification", text: notificationText });
-          $("body").append(notificationTag);
-
-          notificationTag.fadeIn("slow", function () {
-              setTimeout(function () {
-                  notificationTag.fadeOut("slow", function () {
-                      notificationTag.remove();
-                  });
-              }, 1000);
-          });
-      }
-  }
 
 
   const shareData = {
