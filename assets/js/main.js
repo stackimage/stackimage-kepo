47c26e8c13d543d729751840c6098cf04ee8f303
joxiplayer <joxiplayer.id@gmail.com>
2022-01-15 15:08:49 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,86 @@ document.querySelector('#bell-icon').onclick = () => {
 
   }
 
+  //Slider Image Download
+  var slides=document.querySelector('.slider-items').children;
+var nextSlide=document.querySelector(".right-slide");
+var prevSlide=document.querySelector(".left-slide");
+var totalSlides=slides.length;
+var index=0;
+
+nextSlide.onclick=function () {
+    next("next");
+}
+prevSlide.onclick=function () {
+    next("prev");
+}
+
+function next(direction){
+
+  if(direction=="next"){
+     index++;
+      if(index==totalSlides){
+       index=0;
+      }
+  } 
+  else{
+          if(index==0){
+           index=totalSlides-1;
+          }
+          else{
+           index--;
+          }
+   }
+
+ for(i=0;i<slides.length;i++){
+         slides[i].classList.remove("active");
+ }
+ slides[index].classList.add("active");     
+
+}
+
+
+
+// Alert segera hadir
+function showToast(text){
+  var x=document.getElementById("toast");
+  x.classList.add("show");
+  x.innerHTML=text;
+  setTimeout(function(){
+    x.classList.remove("show");
+  },3000);
+}
+
+// Button Download Timer
+const downloadBtn = document.querySelector(".download-btn-timer");
+    const countdown = document.querySelector(".countdown");
+    const pleaseWaitText = document.querySelector(".pleaseWait-text");
+    const manualDownloadText = document.querySelector(".manualDownload-text");
+    const manualDownloadLink = document.querySelector(".manualDownload-link");
+    var timeLeft = 10;
+
+    downloadBtn.addEventListener("click", () => {
+      downloadBtn.style.display = "none";
+      countdown.innerHTML = "Download will begin automatically in <span>" + timeLeft + "</span> seconds."; //for quick start of countdown
+
+      var downloadTimer = setInterval(function timeCount(){
+        timeLeft -= 1;
+        countdown.innerHTML = "Download will begin automatically in <span>" + timeLeft + "</span> seconds.";
+
+        if(timeLeft <= 0){
+          clearInterval(downloadTimer);
+          pleaseWaitText.style.display = "block";
+          let download_href = ""; //enter the downloadable file link URL here
+          window.location.href = download_href;
+          manualDownloadLink.href = download_href;
+
+          setTimeout(() => {
+            pleaseWaitText.style.display = "none";
+            manualDownloadText.style.display = "block";
+          }, 4000);
+        }
+      }, 1000);
+    });
 
   // Alert
 
