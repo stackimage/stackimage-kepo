329872fdd528fcf5f2abd5c00a402b0f50cafa5e
joxiplayer <joxiplayer.id@gmail.com>
2022-01-20 14:39:58 +0700

    first commit

--- File/Download-Design/Design-Bca-Mobile/download.js
+++ File/Download-Design/Design-Bca-Mobile/download.js
@@ -65,6 +65,15 @@ link.appendChild(element.cloneNode(true));
 parent.replaceChild(link, element);
 // share telegram
 
+// Share Gmail
+var element = document.getElementById("Gmail");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://mail.google.com/mail/?view=cm&to=%7Bemail_address%7D&su=Download%20Design%20BCA%20Mobile%20%7C%20Stack%20Image&body=https://www.stackimage.my.id/File/Download-Design/Design-Bca-Mobile/&bcc=%7Bemail_address%7D&cc=%7Bemail_address%7D';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+// Share Gmail
+
 copy.onclick = () => {
   input.select(); //select input value
   if (document.execCommand("copy")) { //if the selected text copy
--- File/Download-Design/Design-Vaksini-Mobile-App/download.js
+++ File/Download-Design/Design-Vaksini-Mobile-App/download.js
@@ -31,21 +31,58 @@ const downloadBtn = document.querySelector(".download-btn-timer");
 
 
 
-    const shareData = {
-      title: 'Stack Image',
-      text: 'Download Design Vaksini Mobile App | Stack Image',
-      url: 'https://www.stackimage.my.id/File/Download-Design/Design-Vaksini-Mobile-App/'
-    }
-  
-    const btn = document.querySelector('button');
-    const resultPara = document.querySelector('.result');
-  
-    // Share must be triggered by "user activation"
-    btn.addEventListener('click', async () => {
-      try {
-        await navigator.share(shareData)
-        resultPara.textContent = 'MDN shared successfully'
-      } catch(err) {
-        resultPara.textContent = 'Error: ' + err
-      }
-    });
+
+
+
+const viewBtn = document.querySelector(".bx"),
+popup = document.querySelector(".popup"),
+close = popup.querySelector(".close"),
+field = popup.querySelector(".field"),
+input = field.querySelector("input"),
+copy = field.querySelector("button");
+viewBtn.onclick = () => {
+popup.classList.toggle("show");
+}
+close.onclick = () => {
+viewBtn.click();
+}
+
+// share wa
+var element = document.getElementById("Whatsapp");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://wa.me/?text=Download Design Vaksini Mobile App | Stack Image https://www.stackimage.my.id/File/Download-Design/Design-Vaksini-Mobile-App/';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+// share wa
+
+// share telegram
+var element = document.getElementById("Telegram");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://telegram.me/share/url?text=Download Design Vaksini Mobile App | Stack Image Image&url=https://www.stackimage.my.id/File/Download-Design/Design-Vaksini-Mobile-App/';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+// share telegram
+
+// Share Gmail
+var element = document.getElementById("Gmail");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://mail.google.com/mail/?view=cm&to=%7Bemail_address%7D&su=Download%20Design%20Vaksini%20Mobile%20App%20%7C%20Stack%20Image&body=https://www.stackimage.my.id/File/Download-Design/Design-Vaksini-Mobile-App/&bcc=%7Bemail_address%7D&cc=%7Bemail_address%7D';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+// Share Gmail
+
+copy.onclick = () => {
+input.select(); //select input value
+if (document.execCommand("copy")) { //if the selected text copy
+  field.classList.add("active");
+  copy.innerText = "Copied";
+  setTimeout(() => {
+    window.getSelection().removeAllRanges(); //remove selection from document
+    field.classList.remove("active");
+    copy.innerText = "Copy";
+  }, 3000);
+}
+}
