fab2cd05c610339a51074a0fab8578dcf341cdbe
joxiplayer <joxiplayer.id@gmail.com>
2022-01-25 16:55:09 +0700

    first commit

--- ads/assets/js/script.js
+++ ads/assets/js/script.js
@@ -1,3 +1,30 @@
+/*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) =>{
+  const openBtn = document.getElementById(openButton),
+  modalContainer = document.getElementById(modalContent)
+  
+  if(openBtn && modalContainer){
+      openBtn.addEventListener('click', ()=>{
+          modalContainer.classList.add('show-modal')
+      })
+  }
+}
+showModal('open-modal','modal-container')
+showModal('open-modal-2','modal-container')
+showModal('open-modal-3','modal-container')
+showModal('open-modal-4','modal-container')
+
+/*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+  const modalContainer = document.getElementById('modal-container')
+  modalContainer.classList.remove('show-modal')
+}
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
+
+
+
 'use strict';
 
 // navbar variables
